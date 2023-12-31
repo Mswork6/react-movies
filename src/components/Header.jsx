247aebc1620df0869a073a4d5c65732b8bacd283
Mswork6 <denis.ch74@mail.ru>
2023-12-31 15:23:58 +0500

    feat: made react task

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <header className="header">
+      <h1 className="header__title">Админка фильмотеки</h1>
+      <button className="header__button">Чашин Денис</button>
+    </header>
+  );
+};
+
+export default Header;
